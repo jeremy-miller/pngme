e3b1e191dccc685ae7b20d23431d79d0b596ceb0
Jeremy Miller <jeremymiller@fastmail.com>
2024-06-06 21:34:29 -0700

    Add cli skeleton

---
+++ src/args.rs
@@ -0,0 +1,73 @@
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
+
+/// Program to hide messages in PNG files
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct App {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Encode a message into a PNG file
+    Encode(EncodeArgs),
+
+    /// Decode a message from a PNG file
+    Decode(DecodeArgs),
+
+    /// Remove a message from a PNG file
+    Remove(RemoveArgs),
+
+    /// Print all messages in a PNG file
+    Print(PrintArgs),
+}
+
+#[derive(Args, Debug)]
+pub struct EncodeArgs {
+    /// Path to input PNG file
+    #[arg(short, long)]
+    file_path: PathBuf,
+
+    /// Chunk type of message
+    #[arg(short, long)]
+    chunk_type: String,
+
+    /// Message to encode
+    #[arg(short, long)]
+    message: String,
+
+    /// Optional path to output PNG file
+    #[arg(short, long)]
+    output_file_path: Option<PathBuf>,
+}
+
+#[derive(Args, Debug)]
+pub struct DecodeArgs {
+    /// Path to input PNG file
+    #[arg(short, long)]
+    file_path: PathBuf,
+
+    /// Chunk type of message
+    #[arg(short, long)]
+    chunk_type: String,
+}
+
+#[derive(Args, Debug)]
+pub struct RemoveArgs {
+    /// Path to input PNG file
+    #[arg(short, long)]
+    file_path: PathBuf,
+
+    /// Chunk type of message
+    #[arg(short, long)]
+    chunk_type: String,
+}
+
+#[derive(Args, Debug)]
+pub struct PrintArgs {
+    /// Path to input PNG file
+    #[arg(short, long)]
+    file_path: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod chunk_type;
 mod commands;
 mod png;
 
+use crate::args::{App, Commands};
+use clap::Parser;
 use color_eyre::eyre::Result;
 use tracing_subscriber::FmtSubscriber;
 
@@ -11,5 +13,21 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let _subscriber = FmtSubscriber::builder().finish();
 
-    todo!()
+    let app = App::parse();
+    match app.command {
+        Commands::Encode(args) => {
+            println!("Encoding with args: {:?}", args);
+        }
+        Commands::Decode(args) => {
+            println!("Decoding with args: {:?}", args);
+        }
+        Commands::Remove(args) => {
+            println!("Removing with args: {:?}", args);
+        }
+        Commands::Print(args) => {
+            println!("Printing with args: {:?}", args);
+        }
+    }
+
+    Ok(())
 }
